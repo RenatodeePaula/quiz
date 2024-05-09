5dd58d821cfd3f7896cc95c3c6dbf2ff0dcc33b3
Renato de Paula~ <renatodepaula.tec@gmail.com>
2024-05-09 12:20:17 -0300

    final project

---
+++ script.js
@@ -0,0 +1,125 @@
+const questions = [
+  {
+    question: "Qual é a capital do Brasil?",
+    choices: ["Brasília", "Rio de Janeiro", "São Paulo", "Salvador"],
+    answer: "Brasília",
+  },
+  {
+    question: "Qual é a capital da Argentina?",
+    choices: ["Buenos Aires", "Brasília", "Lisboa", "Paris"],
+    answer: "Buenos Aires",
+  },
+  {
+    question: "Qual é a capital da França?",
+    choices: ["Roma", "Madri", "Paris", "Londres"],
+    answer: "Paris",
+  },
+  {
+    question: "Qual é a capital da Espanha?",
+    choices: ["Lisboa", "Madri", "Barcelona", "Valência"],
+    answer: "Madri",
+  },
+  {
+    question: "Qual é a capital da Itália?",
+    choices: ["Veneza", "Milão", "Roma", "Nápoles"],
+    answer: "Roma",
+  },
+  {
+    question: "Qual é a capital do Canadá?",
+    choices: ["Toronto", "Vancouver", "Ottawa", "Montreal"],
+    answer: "Ottawa",
+  },
+  {
+    question: "Qual é a capital dos Estados Unidos?",
+    choices: ["Nova York", "Los Angeles", "Chicago", "Washington D.C."],
+    answer: "Washington D.C.",
+  },
+  {
+    question: "Qual é a capital do Reino Unido?",
+    choices: ["Liverpool", "Manchester", "Edimburgo", "Londres"],
+    answer: "Londres",
+  },
+];
+  
+const questionElement = document.querySelector('#question');
+const choiceElements = document.querySelectorAll('.choice');
+const nextButton = document.querySelector('#next');
+const scoreElement = document.querySelector('#score');
+const wrongElement = document.querySelector('#wrong');
+
+let currentQuestion = 0;
+let score = 0;
+let wrong = 0;
+let answerChosen = false;
+
+function loadQuestion() {
+  const currentQuestionData = questions[currentQuestion];
+  questionElement.innerText = currentQuestionData.question;
+
+  const choices = shuffleArray(currentQuestionData.choices);
+
+  choiceElements.forEach( (elem, indice) => {
+    elem.innerText = choices[indice]
+  });
+  answerChosen = false;
+};
+
+function shuffleArray(array) {
+  currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
+
+  while(currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -=1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  };
+  return array;
+};
+
+function checkAnswer(ev) {
+  if(answerChosen) return;
+  answerChosen = true;
+
+  if(ev.target.innerText === questions[currentQuestion].answer) {
+    score++;
+    scoreElement.innerText = `Pontuação: ${score}`;
+    alert('Correto!')
+  } else {
+    wrong++;
+    wrongElement.innerText = `Erros: ${wrong}`
+    alert(`Errado! A resposta correta é ${questions[currentQuestion].answer}`)
+  };
+};
+
+choiceElements.forEach((btn) => {
+  btn.addEventListener('click', checkAnswer);
+});
+
+nextButton.addEventListener('click', () => {
+  if(!answerChosen) {
+    alert("Por favor, responda a pergunta!");
+    return;
+  } else {
+    currentQuestion++
+  };
+
+  if(currentQuestion < questions.length) {
+    loadQuestion();
+  } else{
+    alert(`Fim de Jogo! Você acertoou ${score} de ${questions.length}.`)
+    restartQuiz();
+  };
+});
+
+function restartQuiz() {
+  currentQuestion = 0;
+  score = 0;
+  wrong= 0;
+  scoreElement.innerText = `Pontuação: 0`;
+  wrongElement.innerText = `Erros: 0`;
+};
+loadQuestion();
